96e7beeefde5e3961864465d139be3a10f948df6
Pelafina <decoy110@pm.me>
2026-07-03 15:11:31 +0200

    task3 ch2l4

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,18 @@ class HTMLNode:
         for value in self.props:
             html_text += f" {value}={self.props[value]}"
         return html_text
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(self, tag, value, props)
+
+    def __repr__(self):
+        print(f"Tag = {self.tag}, Value = {self.value}, Props = {self.props}")
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag == None:
+            return self.value
+
+        return f"<{self.tag} {self.super().prop_to_html()}>{self.value}</{self.tag}>"
