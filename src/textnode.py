47171e45682ad12ab908707b10ba1a274dcbb00a
Pelafina <decoy110@pm.me>
2026-07-04 16:08:18 +0200

    start of ch2l6

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -16,6 +17,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+    def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+        if self.text_type not in TextType:
+            raise ValueError("Wrong TextType")
+        texttype_to_tag = {
+                "plain": ""
+                "bold": "b"
+                "italics": "i"
+                "code": "code"
+                }
+        if self.text_type in texttype_to_tag:
+            return f"<{texttype_to_tag[self.text_type]}>{self.text}</{texttype_to_tag[self.text_type]}>"
+        elif self.text_type == "link":
+            return f"<a>[{self.text}]({self.url})</a>"
+        elif self.text_type == "image":
+            return f"![{self.text}]({self.url})"
 
 class TextType(Enum):
     PLAIN = "plain"
