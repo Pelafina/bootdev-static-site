893ff342b3913ffe8bbc9e472f69a66b35d83df4
Pelafina <decoy110@pm.me>
2026-07-10 19:41:35 +0200

    end of ch3l1

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,2 +1,25 @@
-import TextNode
+from textnode import TextNode, TextType  
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    return_list = []
+    delimiter_to_texttype = {
+            "**" : TextType.BOLD,
+            "_" : TextType.ITALIC,
+            "`" : TextType.CODE,
+            }
+    if old_nodes.text_type != TextType.TEXT:
+        return_list.append(old_nodes)
+        return return_list
+    if delimiter not in old_nodes.text:
+        raise Exception("No valid delimiter found in text")
+
+    split_text_list = old_nodes.text.split(delimiter)
+    if len(split_text_list) != 3:
+        raise Exception("No closing delimiter found in text")
+
+    return_list = [
+            TextNode(split_text_list[0], TextType.TEXT),
+            TextNode(split_text_list[1], delimiter_to_texttype[delimiter]),
+            TextNode(split_text_list[2], TextType.TEXT)
+            ]
+    
+    return return_list
---
+++ src/test_delimiter.py
@@ -0,0 +1,33 @@
+import unittest
+from split_delimiter import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+class TestDelimiter(unittest.TestCase):
+    def test_eq(self):
+        italic_delimiter_expected = [
+                TextNode("delimited before italics", TextType.TEXT),
+                TextNode("delimited italics", TextType.ITALIC),
+                TextNode("delimited after italics", TextType.TEXT)
+                ]
+        bold_delimiter_expected = [
+                TextNode("delimited before bold", TextType.TEXT),
+                TextNode("delimited bold", TextType.BOLD),
+                TextNode("delimited after bold", TextType.TEXT)
+                ]
+        code_delimiter_expected = [
+                TextNode("delimited before code", TextType.TEXT),
+                TextNode("delimited code", TextType.CODE),
+                TextNode("delimited after code", TextType.TEXT)
+                ]
+        
+        italic_delimiter_test_node = TextNode("delimited before italics_delimited italics_delimited after italics", TextType.TEXT)
+        bold_delimiter_test_node = TextNode("delimited before bold**delimited bold**delimited after bold", TextType.TEXT)
+        code_delimiter_test_node = TextNode("delimited before code`delimited code`delimited after code", TextType.TEXT)
+
+        italic_delimiter_test_node_delimited = split_nodes_delimiter(italic_delimiter_test_node, "_", italic_delimiter_test_node.text_type)
+        bold_delimiter_test_node_delimited = split_nodes_delimiter(bold_delimiter_test_node, "**", bold_delimiter_test_node.text_type)
+        code_delimiter_test_node_delimited = split_nodes_delimiter(code_delimiter_test_node, "`", code_delimiter_test_node.text_type)
+
+        self.assertEqual(italic_delimiter_test_node_delimited, italic_delimiter_expected)
+        self.assertEqual(bold_delimiter_test_node_delimited, bold_delimiter_expected)
+        self.assertEqual(code_delimiter_test_node_delimited, code_delimiter_expected)
