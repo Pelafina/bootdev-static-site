c461f6ac535c886b9c8021f292ef858f2382249d
Pelafina <decoy110@pm.me>
2026-07-12 21:02:53 +0200

    node splitting despair

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -7,10 +7,26 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
 
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
     # todo: extract links, after extraction split textnode text based on extracted text, create new textnodes with extracted text
     if not isinstance(old_nodes, list):
         old_nodes = [old_nodes]
-    
-    extracted_links = extract_markdown_links(old_nodes)
+
+    #Extracts all links from the TextNodes and saves them in a list for later use    
+    extracted_links = [] 
+    for node in old_nodes:
+        extracted_links.append(extract_markdown_links(node.text))
+
+    for node in old_nodes:
+        for link_text in extracted_links:
+            if link_text not in node.text:
+                continue
+            text_without_link = node.text.split(f"[{link_text[0]}]({link_text[1]})")
+            new_nodes.append(
+                    TextNode(text_without_link[0], TextType.TEXT),
+                    TextNode(link_text[0], TextType.LINK, link_text[1])
+                    )
+            
+
 
 
