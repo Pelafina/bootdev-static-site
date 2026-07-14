74ebab6d0f428cfc13f18dbbde28c33a77857486
Pelafina <decoy110@pm.me>
2026-07-15 00:22:47 +0200

    middle fo ch4l2, code block doesnt work sadge

--- src/block.py
+++ src/block.py
@@ -1,7 +1,41 @@
+from enum import Enum
 def markdown_to_blocks(markdown: str) -> list[str]:
-    raw_blocks = markdown.split("\n")
+    raw_blocks = markdown.split("\n\n")
     blocks = []
     for block in raw_blocks:
-        if block != "":
-            blocks.append(block.strip())
+        if block == "":
+            continue
+        blocks.append(block.strip())
     return blocks
+
+def block_to_block_type(block: str) -> BlockType:
+    if block.startswith("#"):
+        return BlockType.HEADING
+    elif block.startswith(">"):
+        for i in range(len(block.split("\n")) -1):
+            if not block.split("\n")[i].startswith(">"):
+                raise Exception("Quote Block is not formatted correctly")
+        return BlockType.QUOTE
+    elif block.startswith("-"):
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("```\n"):
+        if block.split("\n")[-1].startswith("```"):
+            return BlockType.CODE
+        else:
+            raise Exception("Code Block is not closed")
+    elif block.startswith(f"1."):
+        for i in range(len(block.split("\n"))-1):
+            if not block.split("\n")[i].startswith(f"{i + 1}."):
+                raise Exception("Ordered List is not formatted correctly")
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE =  "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
---
+++ src/test_block_to_block_type.py
@@ -0,0 +1,26 @@
+import unittest
+from block import BlockType, block_to_block_type
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_eq(self):
+        paragraph = "Paragraph" 
+        heading = "# Heading"
+        code = """
+```
+code
+```"""
+        quote = "> quote"
+        unordered_list = """
+- List item
+- List item2"""
+        ordered_list = """
+1. Ordered item
+2. Ordered item
+3. Ordered item"""
+
+        self.assertEqual(BlockType.PARAGRAPH, block_to_block_type(paragraph))
+        self.assertEqual(BlockType.HEADING, block_to_block_type(heading))
+        self.assertEqual(BlockType.CODE, block_to_block_type(code))
+        self.assertEqual(BlockType.QUOTE, block_to_block_type(quote))
+        self.assertEqual(BlockType.UNORDERED_LIST, block_to_block_type(unordered_list))
+        self.assertEqual(BlockType.ORDERED_LIST, block_to_block_type(ORDERED_LIST))
