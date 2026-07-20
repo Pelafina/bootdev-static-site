d9f9d4c02f59c7d2d01b03643372e11f6bb7ae6b
Pelafina <decoy110@pm.me>
2026-07-20 06:27:23 +0200

    start of ch5l3

--- src/generate_page.py
+++ src/generate_page.py
@@ -18,16 +18,30 @@ def generate_page(from_path, template_path, dest_path):
                 return file.read()
         else:
             raise Exception(f"{path} is not a file")
+    
+    def get_all_content_files(path) -> list[file_path:str]:
+        content_files = os.listdir(path)
+        file_paths = []
+        for file in content_files:
+            if os.isfile(path):
+                file_paths.append(file)
+            else:
+                file_paths.append(get_all_content_files(file))
+        return file_paths
+
 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    markdown = read_file_contents(from_path)
-    template = read_file_contents(template_path)
-    html_string = markdown_to_html_node(markdown).to_html()
-    page_title = extract_title(markdown)
-    html_page = template.replace("{{ Title }}", page_title).replace("{{ Content }}", html_string)
+    content_files = get_all_content_files(from_path)
+    #todo: go through each file present in /content/ and generate folders and indexes in /public/
+    for file in content_files:
+        markdown = read_file_contents(file)
+        template = read_file_contents(template_path)
+        html_string = markdown_to_html_node(markdown).to_html()
+        page_title = extract_title(markdown)
+        html_page = template.replace("{{ Title }}", page_title).replace("{{ Content }}", html_string)
 
-    if not os.path.exists(dest_path):
-        os.mkdir(dest_path)
-    with open(os.path.join(dest_path, "index.html"), "w") as file:
-        file.write(html_page)
+        if not os.path.exists(dest_path):
+            os.mkdir(dest_path)
+        with open(os.path.join(dest_path, "index.html"), "w") as file:
+            file.write(html_page)
 
--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import os
 
 def main():
     copy_static_to_public()
-    content = os.path.abspath("./content/index.md")
+    content = os.path.abspath("./content")
     template = os.path.abspath("./template.html")
     dest_path = os.path.abspath("./public")
     generate_page(content, template, dest_path)
