8a76a62016b4f6744ba7b0694ed227476807e9be
Pelafina <decoy110@pm.me>
2026-07-20 04:42:58 +0200

    Copy static directory contents into public directory contents functioning

---
+++ src/copy_to_public.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+def copy_static_to_public():
+    path_to_public_dir = os.path.abspath(os.path.join(".", "public"))
+    path_static_dir = os.path.abspath(os.path.join(".", "static"))
+    if os.path.exists(path_to_public_dir):
+        shutil.rmtree(path_to_public_dir) 
+    os.mkdir(path_to_public_dir)
+    if not os.path.exists(path_static_dir):
+        raise Exception("No static directory from which to copy exists.")
+    else:
+        copy_file_contents(path_static_dir, path_to_public_dir)
+
+def copy_file_contents(src_path, dst_path):
+        files_to_copy = os.listdir(src_path)
+        for file in files_to_copy:
+            file_path = os.path.join(src_path, file)
+            if os.path.isfile(file_path):
+                shutil.copy(file_path, dst_path)
+            else:
+                os.mkdir(os.path.join(dst_path, file))
+                copy_file_contents(file_path, os.path.join(dst_path, file))
+
+copy_static_to_public()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from textnode import TextNode
 from textnode import TextType
+from copy_to_public import copy_static_to_public
 
 def main():
     copy_static_to_public()
 
-def copy_static_to_public():
+
+    
     #todo everything :)
 
 main()
